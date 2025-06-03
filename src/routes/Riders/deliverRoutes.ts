b5af5e08def79524a2e7f413fc2ce56e98ea2c2d
VerseX-Global <info@versexglobal.com>
2025-06-03 08:25:33 -0500

    Fixed Riders Route

--- src/routes/Riders/deliverRoutes.ts
+++ src/routes/Riders/deliverRoutes.ts
@@ -1,12 +1,12 @@
-// src/routes/userRoutes.ts
-import express from 'express';
-import { authenticateJWT } from '../../middlewares/authMiddleware/authenticationMiddleware';
-// import { acceptDelivery, pickDelivery, rejectDelivery, viewAllDelivery, viewSingleDelivery } from '../../controllers/RiderControllers/deliveryController';
+// // src/routes/userRoutes.ts
+// import express from 'express';
+// import { authenticateJWT } from '../../middlewares/authMiddleware/authenticationMiddleware';
+// // import { acceptDelivery, pickDelivery, rejectDelivery, viewAllDelivery, viewSingleDelivery } from '../../controllers/RiderControllers/deliveryController';
 
-export const riderDeliveryRouter = express.Router();
+// export const riderDeliveryRouter = express.Router();
 
-// riderDeliveryRouter.get('/pickup-delivery', authenticateJWT, pickDelivery);
-// riderDeliveryRouter.get('/all-delivery', authenticateJWT, viewAllDelivery);
-// riderDeliveryRouter.get('/single-delivery', authenticateJWT, viewSingleDelivery);
-// riderDeliveryRouter.put('/accept-delivery', authenticateJWT, acceptDelivery);
-// riderDeliveryRouter.put('/reject-delivery', authenticateJWT, rejectDelivery);
+// // riderDeliveryRouter.get('/pickup-delivery', authenticateJWT, pickDelivery);
+// // riderDeliveryRouter.get('/all-delivery', authenticateJWT, viewAllDelivery);
+// // riderDeliveryRouter.get('/single-delivery', authenticateJWT, viewSingleDelivery);
+// // riderDeliveryRouter.put('/accept-delivery', authenticateJWT, acceptDelivery);
+// // riderDeliveryRouter.put('/reject-delivery', authenticateJWT, rejectDelivery);
